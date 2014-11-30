b616635b9b91570fea804ca704dc89467358343d
Ben2001 <ben@ordi-cretin.(none)>
2014-11-30 14:35:11 +0100

    add function to resize yellowblock depending of screen size

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,7 +24,7 @@ $(document).ready(function() {
 	// show the navbar when scrolling below half homepage
 	$(document).scroll(function() {
 		var navbar = $("#header").offset();
-		var navbartop = navbar.top
+		var navbartop = navbar.top;
 		if (navbartop < 420) {
 			$("#header").hide();
 		}
@@ -36,29 +36,29 @@ $(document).ready(function() {
 	// scrolldown 1st yellowblock
 	$('#plus1').click(function() {
 		$('#logo_jb').hide();
-		$('#yb1').slideDown("slow");
 		$('#plus1').hide();
 		$('#moins1').show();
+		$('#yb1').slideDown("slow").promise().then(enlargeYb1);
 	});
 
 	$('#moins1').click(function() {
-		$('#yb1').slideUp("slow");
 		$('#moins1').hide();
 		$('#plus1').show();
+		$('#yb1').slideUp("slow").promise().then(shrinkYb1);
 		$('#logo_jb').delay(660).show(0);
 	});
 
 	// scrolldown 2nd yellowblock
 	$('#plus2').click(function() {
-		$('#yb2').slideDown("slow");
 		$('#plus2').hide();
 		$('#moins2').show();
+		$('#yb2').slideDown("slow").promise().then(enlargeYb2);
 	});
 
 	$('#moins2').click(function() {
-		$('#yb2').slideUp("slow");
 		$('#moins2').hide();
 		$('#plus2').show();
+		$('#yb2').slideUp("slow").promise().then(shrinkYb2);
 	});
 
 
@@ -80,3 +80,54 @@ $(document).ready(function() {
 
 });
 
+
+// Les 4 fonctions qui suivent servent à redimensionner les yellowblock des
+// pages 'quisommesnous' et 'nosactions' selon la taille du device
+// Ces fonctions peuvent être refactorer mais la flemme m'en a empêchée :)
+function enlargeYb1()
+{
+	var quiSommesNousHeight= $('#quisommesnous .yellowblock').height() + $('#quisommesnous .container').height();
+	var diff = quiSommesNousHeight - $('#quisommesnous').height();
+	if ( quiSommesNousHeight < $('#quisommesnous').height() ) {
+		$('#quisommesnous .yellowblock').css('height', '100%')
+			} else {
+			$('#quisommesnous').css('height', quiSommesNousHeight + 'px');
+			for (var index in classesCss = ['#nosprojets', '#nosactions', '#presse', '#etvous', '#souteneznous']){
+				$(classesCss[index]).css('top', ($(classesCss[index]).offset().top + diff) + 'px');
+			}
+	}
+}
+
+function shrinkYb1()
+{
+	regularHeight = $('#nosprojets').height();
+	diff = $('#quisommesnous').height() - $('#nosprojets').height();
+	$('#quisommesnous').height(regularHeight);
+	for (var index in classesCss = ['#nosprojets', '#nosactions', '#presse', '#etvous', '#souteneznous']){
+		$(classesCss[index]).css('top', ($(classesCss[index]).offset().top - diff) + 'px');
+	};
+};
+
+function enlargeYb2()
+{
+	var nosActionsHeight = $('#nosactions .yellowblock').height() + $('#nosactions .container').height();
+	var diff = nosActionsHeight - $('#nosactions').height();
+	if ( nosActionsHeight < $('#nosactions').height() ) {
+		$('#nosactions .yellowblock').css('height', '100%')
+			} else {
+			$('#nosactions').css('height', nosActionsHeight + 'px');
+			for (var index in classesCss = ['#presse', '#etvous', '#souteneznous']){
+				$(classesCss[index]).css('top', ($(classesCss[index]).offset().top + diff) + 'px');
+			}
+	}
+}
+
+function shrinkYb2()
+{
+	regularHeight = $('#nosprojets').height();
+	diff = $('#nosactions').height() - $('#nosprojets').height();
+	$('#nosactions').height(regularHeight);
+	for (var index in classesCss = ['#presse', '#etvous', '#souteneznous']){
+		$(classesCss[index]).css('top', ($(classesCss[index]).offset().top - diff) + 'px');
+	};
+};
