3ba88bf701da37fe7bc2ff645965ef6eca539ce7
Ben2001 <ben@ordi-cretin.(none)>
2015-08-05 23:46:34 +0200

    adds Donor to activeadmin

--- app/models/donor.rb
+++ app/models/donor.rb
@@ -1,3 +1,10 @@
 class Donor < ActiveRecord::Base
 	has_many :donations, inverse_of: :donor
+
+	ActiveAdmin.register Donor do
+		permit_params :email, :company, :gender, :firstname, :lastname,
+									:address, :additional_address_details, :zipcode,
+									:city, :country, :created_at
+		actions :all
+	end
 end
