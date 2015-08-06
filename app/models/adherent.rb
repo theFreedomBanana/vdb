8141caeaa395f3534377667771148de2075d4add
Ben2001 <ben@ordi-cretin.(none)>
2015-08-06 12:11:19 +0200

    subscriptions are created and destroyed simultaneously as adherents

--- app/models/adherent.rb
+++ app/models/adherent.rb
@@ -1,5 +1,6 @@
 class Adherent < ActiveRecord::Base
-	has_many :subscriptions, inverse_of: :adherent
+	has_many :subscriptions, :dependent => :destroy, inverse_of: :adherent
+	accepts_nested_attributes_for :subscriptions, allow_destroy: true
 	
 	ActiveAdmin.register Adherent do
 		permit_params :email, :company, :gender, :firstname, :lastname,
