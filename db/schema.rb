004ab02eb60fb2e9538621d2fe6130defbd66a37
Ben2001 <ben@ordi-cretin.(none)>
2015-08-06 20:44:46 +0200

    rename amount to subscription_amount in table subscription

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150806182650) do
+ActiveRecord::Schema.define(version: 20150806184200) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -92,7 +92,7 @@ ActiveRecord::Schema.define(version: 20150806182650) do
   end
 
   create_table "subscriptions", force: true do |t|
-    t.integer  "amount"
+    t.integer  "subscription_amount"
     t.datetime "ends_at"
     t.datetime "created_at"
     t.datetime "updated_at"
