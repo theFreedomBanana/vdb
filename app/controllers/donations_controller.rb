f2b48c630566b6856e8db5041237e9779a7584ef
Ben2001 <ben@ordi-cretin.(none)>
2015-08-06 14:54:25 +0200

    adds Donation model, view to create it, and the ability to destroy it simultaneously with it's donor's parent

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -5,15 +5,16 @@ class DonationsController < ApplicationController
 	end
 
 	def create
-		@donor = Donor.create(donor_params)
+		donor = Donor.create(donor_params)
 		redirect_to :back
 	end
 
 	private
 
-		def donor_params
-  		params.permit(:email, :company, :gender, :firstname ,:lastname, 
-    	:address, :additional_address_details, :zipcode, :city, :country)
-  	end
+	def donor_params
+		params.permit(:email, :company, :gender, :firstname ,:lastname, 
+			:address, :additional_address_details, :zipcode, :city, :country, 
+			donations_attributes: [:donation_amount, :id, :_destroy])
+	end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150804192502) do
+ActiveRecord::Schema.define(version: 20150806182650) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -60,6 +60,13 @@ ActiveRecord::Schema.define(version: 20150804192502) do
 
   add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true
   add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+
+  create_table "donations", force: true do |t|
+    t.integer  "donation_amount"
+    t.integer  "donor_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "donors", force: true do |t|
     t.string   "email"
