2f11df5430348c7ac34cc85468f12c82d447c506
Ben2001 <ben@ordi-cretin.(none)>
2015-08-06 20:50:54 +0200

    adds monthly boolean to table subscriptions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150806184200) do
+ActiveRecord::Schema.define(version: 20150806184608) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -97,6 +97,7 @@ ActiveRecord::Schema.define(version: 20150806184200) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "adherent_id"
+    t.boolean  "monthly"
   end
 
 end
