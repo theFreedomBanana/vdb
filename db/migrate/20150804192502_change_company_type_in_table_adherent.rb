1693dcfd886a9408298b849960e7b2e4f1cc03cb
theFreedomBanana <benjamin.mahmoudi@gmail.com>
2015-10-29 18:35:29 +0100

    change migration 'change_company_type_in_table_adherent' to allow boolean conversion on Scalingo

--- db/migrate/20150804192502_change_company_type_in_table_adherent.rb
+++ db/migrate/20150804192502_change_company_type_in_table_adherent.rb
@@ -1,5 +1,5 @@
 class ChangeCompanyTypeInTableAdherent < ActiveRecord::Migration
   def change
-  	change_column :adherents, :company,  :boolean
+  	change_column :adherents, :company, 'boolean USING CAST(company AS boolean)'
   end
 end
